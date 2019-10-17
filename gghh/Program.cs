0e9c0775e05efe110240310ea82ab51bf396789a
ArzuHa <53233132+ArzuHa@users.noreply.github.com>
2019-10-17 18:10:08 +0400

    Add files via upload

---
+++ gghh/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gghh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, temp,rz, rev = 0;
+            int sum = 0;
+            Console.WriteLine("Reqem daxil edin:");
+            n = int.Parse(Console.ReadLine());
+
+            // reverse any number
+
+            while (n != 0)
+            {
+                rev = (rev * 10) + (n % 10);
+                n = n / 10;
+            }
+            n = rev;
+
+            // split any number
+            Console.WriteLine("\n");
+
+
+            while (n != 0)
+            {
+                temp = n % 10;
+                n = n / 10;
+                for (rz = 1; rz > temp; rz++) ;
+                if (temp%2==0)
+                {
+                    sum += temp;
+                }
+                Console.WriteLine(temp);
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
